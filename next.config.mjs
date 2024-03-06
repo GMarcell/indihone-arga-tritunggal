118c6fc3a947e9b1910c2db92fcca2a2c4f57928
Grand Marcell <grand1310marcell@gmail.com>
2024-03-06 16:53:46 +0700

    adjust deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // delete this if you want to run development server
+  // this code for make the nextjs App become static website
+  output: 'export'
+};
 
 export default nextConfig;
